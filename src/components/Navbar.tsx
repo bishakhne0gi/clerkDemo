62a3a3ce8725a86811cac6b68c9f50afd1e7d4f8
bishakhne0gi <bneogi102002@gmail.com>
2024-01-31 15:12:47 +0530

    clerk auth & collab integration

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,17 +14,17 @@ const Navbar = () => {
           <ModeToggle />
         </div>
         <div className='basis-1/2 flex justify-center space-x-[10px] '>
-          <NavLink to='/' className='hover:text-[#3f3f3f]'>
+          <NavLink to='' className='hover:text-[#3f3f3f]'>
             Home
           </NavLink>
-          <NavLink to='/collab' className='hover:text-[#3f3f3f]'>
+          <NavLink to='collab' className='hover:text-[#3f3f3f]'>
             Collaboration
           </NavLink>
         </div>
         <div className='flex basis-1/3 justify-end space-x-[10px] hover:text-[#3f3f3f]'>
           {user ? (
             <>
-              <NavLink to='/dashboard'>
+              <NavLink to='dashboard'>
                 <span className='text-[#ACACAC] hover:text-[#3f3f3f]'>
                   {email}
                 </span>
